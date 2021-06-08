64dde83b628a0b98becf9c5dcb95ae2a3b3a4f18
marghoob <marghoobahmad0344@gmail.com>
2021-06-08 10:33:17 -0400

    push allwoed host

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(_f*trf+ru9m!t2h$=x4#7@c2fado6&gal-)i!+$xu*))mwzrn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
